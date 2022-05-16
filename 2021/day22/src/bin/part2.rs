5c8ffea00239a642145cc09dcac39f25e777f585
Rami Jebara <rami.jebara@hyperviewhq.com>
2022-05-15 20:23:15 -0700

    day22 part2 solution

--- 2021/day22/src/bin/part2.rs
+++ 2021/day22/src/bin/part2.rs
@@ -1,3 +1,4 @@
+/* Theory: https://en.wikipedia.org/wiki/Inclusion%E2%80%93exclusion_principle */
 use std::{
     fmt,
     fs::File,
@@ -5,16 +6,15 @@ use std::{
 };
 
 fn main() {
-    const OFF: &str = "off";
     const ON: &str = "on";
 
-    let data_lines = if let Ok(file) = File::open("./src/bin/sample2.txt") {
+    let data_lines = if let Ok(file) = File::open("./src/bin/data.txt") {
         io::BufReader::new(file).lines()
     } else {
         return;
     };
 
-    let mut operations: Vec<Cuboid> = Vec::new();
+    let mut instructions: Vec<Cuboid> = Vec::new();
 
     for line in data_lines {
         if let Ok(x) = line {
@@ -59,121 +59,40 @@ fn main() {
 
             let c = Cuboid::new(operation, x_min, x_max, y_min, y_max, z_min, z_max);
 
-            operations.push(c);
+            instructions.push(c);
         }
     }
 
-    let mut compressed_operations: Vec<Cuboid> = Vec::new();
+    // (cuboid, add or subtract)
+    // add = true, subtract = false
+    let mut processed_instructions: Vec<(Cuboid, isize)> = Vec::new();
 
-    // reverse to create a stack
-    operations.reverse();
+    for instruction in instructions {
+        // create temp list to hold intersections and on cubes
+        let mut temp_list: Vec<(Cuboid, isize)> = Vec::new();
 
-    loop {
-        if operations.len() == 0 {
-            break;
-        }
-
-        // pop an operation from the stack
-        let cur = operations.pop().unwrap();
-
-        // handle special case of first on operation
-        if compressed_operations.len() == 0 {
-            if cur.operation == ON {
-                compressed_operations.push(cur);
-            }
-            continue;
-        }
-
-        // intersect and process current operation with previous ones to
-        // figure out which cubes to keep on
-
-        compressed_operations.reverse();
-        let mut x_list: Vec<Cuboid> = Vec::new();
-
-        // remove all the pieces that intersect with current operation
-        loop {
-            if compressed_operations.len() == 0 {
-                break;
-            }
-
-            let prev = compressed_operations.pop().unwrap();
-
-            // if there is no intersection push previous operation
-            // and continue
-            if !prev.intersect(&cur) {
-                x_list.push(prev);
+        // check intersections with processed instructions
+        for processed_instruction in &processed_instructions {
+            if let Some(x) = instruction.intersect(&processed_instruction.0) {
+                let i = (x, processed_instruction.1 * -1);
+                temp_list.push(i);
+            } else {
                 continue;
             }
-
-            if prev.operation == OFF {
-                x_list.push(prev);
-                continue;
-            }
-
-            // if we are at this point then we build an "off" cuboid of
-            // the intersection to avoid double counting
-            let i_x_min = if prev.x_min >= cur.x_min {
-                prev.x_min
-            } else {
-                cur.x_min
-            };
-            let i_x_max = if prev.x_max <= cur.x_max {
-                prev.x_max
-            } else {
-                cur.x_max
-            };
-            let i_y_min = if prev.y_min >= cur.y_min {
-                prev.y_min
-            } else {
-                cur.y_min
-            };
-            let i_y_max = if prev.y_max <= cur.y_max {
-                prev.y_max
-            } else {
-                cur.y_max
-            };
-            let i_z_min = if prev.z_min >= cur.z_min {
-                prev.z_min
-            } else {
-                cur.z_min
-            };
-            let i_z_max = if prev.z_max <= cur.z_max {
-                prev.z_max
-            } else {
-                cur.z_max
-            };
-
-            let c = Cuboid::new(
-                OFF.to_string(),
-                i_x_min,
-                i_x_max,
-                i_y_min,
-                i_y_max,
-                i_z_min,
-                i_z_max,
-            );
-            x_list.push(prev);
-            x_list.push(c);
         }
 
-        // add current operation to to list if on
-        if cur.operation == ON {
-            x_list.push(cur);
+        if instruction.operation == ON {
+            temp_list.push((instruction, 1));
         }
 
-        // reassign the list back to on_cubiods
-        compressed_operations = x_list;
+        processed_instructions.append(&mut temp_list);
     }
 
     // print result
     let mut result: isize = 0;
-    compressed_operations.iter().for_each(|c| {
-        if c.operation == ON {
-            result += c.num_of_cubes();
-        } else {
-            //result -= c.num_of_cubes();
-        }
-        println!("{c}");
+
+    processed_instructions.iter().for_each(|(c, multiplier)| {
+        result += c.num_of_cubes() * multiplier;
     });
 
     println!("Result: {}", result);
@@ -213,18 +132,40 @@ impl Cuboid {
 
     // Two cuboids intersect when the minimum coordinate of one
     // is smaller than the maximum of the other for all axis
-    pub fn intersect(&self, c: &Cuboid) -> bool {
-        self.x_min <= c.x_max
-            && self.y_min <= c.y_max
-            && self.z_min <= c.z_max
-            && c.x_min <= self.x_max
-            && c.y_min <= self.y_max
-            && c.z_min <= self.z_max
+    pub fn intersect(&self, other: &Cuboid) -> Option<Self> {
+        if self.x_min <= other.x_max
+            && self.y_min <= other.y_max
+            && self.z_min <= other.z_max
+            && other.x_min <= self.x_max
+            && other.y_min <= self.y_max
+            && other.z_min <= self.z_max
+        {
+            let x_min = isize::max(self.x_min, other.x_min);
+            let x_max = isize::min(self.x_max, other.x_max);
+            let y_min = isize::max(self.y_min, other.y_min);
+            let y_max = isize::min(self.y_max, other.y_max);
+            let z_min = isize::max(self.z_min, other.z_min);
+            let z_max = isize::min(self.z_max, other.z_max);
+
+            Some(Cuboid::new(
+                "on".to_string(), // This is hardcoded. Can be refactored to be better
+                x_min,
+                x_max,
+                y_min,
+                y_max,
+                z_min,
+                z_max,
+            ))
+        } else {
+            None
+        }
     }
 
     // calculate size
     pub fn num_of_cubes(&self) -> isize {
-        ((self.x_max - self.x_min) * (self.y_max - self.y_min) * (self.z_max - self.z_min)).abs()
+        (self.x_max - self.x_min + 1)
+            * (self.y_max - self.y_min + 1)
+            * (self.z_max - self.z_min + 1)
     }
 }
 
