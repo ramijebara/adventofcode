aefd331bda64a4e6b7b981d6cf15c53263f4c4de
Rami Jebara <rami.jebara@hyperviewhq.com>
2022-12-01 19:32:50 -0800

    2022 - day1

---
+++ 2022/day01/src/bin/part2.rs
@@ -0,0 +1,34 @@
+use std::{io::{self, BufRead}, fs::File};
+
+fn main() {
+    let data_lines = if let Ok(file) = File::open("./src/bin/data.txt") {
+        io::BufReader::new(file).lines()
+    } else {
+        println!("Error reading line");
+        return;
+    };
+
+    let mut calories: Vec<usize> = Vec::new();
+    let mut running_sum = 0;
+    for line in data_lines {
+        if let Ok(x) = line {
+            if let Ok(line_value) = x.parse::<usize>() {
+                running_sum += line_value;
+            } else {
+                calories.push(running_sum);
+                running_sum = 0;
+            }
+        }
+    }
+    calories.sort();
+
+    let mut top3 = Vec::new();
+
+    for _ in 0..3 {
+        match calories.pop() {
+            Some(x) => top3.push(x),
+            None => break
+        }
+    }
+    println!("top3: {:?}", top3.iter().fold(0, |acc, x| acc + x));
+}
